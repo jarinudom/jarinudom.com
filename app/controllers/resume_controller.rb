9769cb45398af2b098f0650bf8ffb1d090e5669a
Jarin Udom <jarin@robotmo.de>
2010-03-15 00:19:50 -0700

    Grab resume and jobs

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,5 +1,8 @@
 class ResumeController < ApplicationController
+  
   def index
+    @resume = Resume.first
+    @jobs = Job.order("(case WHEN end_date IS NULL THEN 1 ELSE 0 END) DESC, end_date DESC, start_date DESC")
   end
 
 end
