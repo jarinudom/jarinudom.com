7c8974c22f2a81eaca4eaf3166f83b8a2ab61b23
Jarin Udom <jarin@robotmo.de>
2010-03-15 01:46:53 -0700

    Add skills to resume page

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -3,6 +3,7 @@ class ResumeController < ApplicationController
   def index
     @resume = Resume.first
     @jobs = Job.order("(case WHEN end_date IS NULL THEN 1 ELSE 0 END) DESC, end_date DESC, start_date DESC")
+    @skills = Skill.order("level ASC, name ASC")
   end
 
 end
