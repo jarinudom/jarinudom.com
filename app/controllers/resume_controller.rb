c41c99b601ad1a6fd68347559f01d0190847a4ca
Jarin Udom <jarin@robotmo.de>
2010-03-15 02:57:06 -0700

    Added certifications to resume page

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -5,6 +5,7 @@ class ResumeController < ApplicationController
     @jobs = Job.order("(case WHEN end_date IS NULL THEN 1 ELSE 0 END) DESC, end_date DESC, start_date DESC")
     @skills = Skill.order("level ASC, name ASC")
     @educations = Education.order("date DESC")
+    @certifications = Certification.order("(case WHEN end_date IS NULL THEN 1 ELSE 0 END) DESC, end_date DESC, start_date DESC")
   end
 
 end
