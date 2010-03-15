f9c3f274d19d016da3eb4518ebda1f332f5fd774
Jarin Udom <jarin@robotmo.de>
2010-03-15 01:46:06 -0700

    Fixed dates in Job blueprint

--- features/support/blueprints.rb
+++ features/support/blueprints.rb
@@ -22,8 +22,8 @@ Job.blueprint do
   position    { Sham.position }
   employer    { Sham.employer }
   location    { Sham.location }
-  start_date  { Date.new.years_ago(2) }
-  end_date    { Date.new.years_ago(1) }
+  start_date  { Time.now.years_ago(2) }
+  end_date    { Time.now.years_ago(1) }
   description { Sham.description }
 end
 
